58783fe0c28e6bf65a53808786f85489b8b6066b
maicondouglas9 <maikdougla421@gmail.com>
2025-09-15 18:53:13 -0300

    Add files via upload

--- koi_reservas/agendar-mesa.php
+++ koi_reservas/agendar-mesa.php
@@ -6,7 +6,7 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-sRIl4kxILFvY47J16cr9ZwB07vP4J8+LH7qKQnuqkuIAvNWLzeN8tE5YBujZqJLB" crossorigin="anonymous">
     <link rel="stylesheet" href="css/estilos.css">
     <link href="https://fonts.googleapis.com/css2?family=Rajdhani:wght@400;600&display=swap" rel="stylesheet">
-    <title>Contato</title>
+    <title>Reserva</title>
     <link rel="icon" href="img/logoa.png" type="image/png">
 </head>
 <body>
