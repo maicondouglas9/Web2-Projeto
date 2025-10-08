9e779283585d555195c0554efd7018d668e43e19
maicondouglas9 <maikdougla421@gmail.com>
2025-10-08 09:32:14 -0300

    Atualizar o alterar_endereco.php

--- koi_reservas/alterar_endereco.php
+++ koi_reservas/alterar_endereco.php
@@ -1,21 +1,27 @@
 <?php
 require_once __DIR__ . "/src/Conexao/Conexao.php";
 
+
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $id = intval($_POST["id_pedido"]);
     $novo_endereco = $_POST["novo_endereco"];
 
+
     $bd = new Conexao();
     $con = $bd->getMysqli();
+
 
     $sql = "UPDATE pedidos SET endereco = ? WHERE id_pedido = ?";
     $stmt = $con->prepare($sql);
     $stmt->bind_param("si", $novo_endereco, $id);
 
+
     if ($stmt->execute()) {
-        echo "<script>alert('Endereço atualizado com sucesso!'); window.location='gerenciar_pedido.php';</script>";
+        echo "<script>alert('Endereço atualizado com sucesso!'); window.location='pedidos.php';</script>";
     } else {
         echo "<script>alert('Erro ao atualizar endereço.'); history.back();</script>";
     }
 }
 ?>
+
+
